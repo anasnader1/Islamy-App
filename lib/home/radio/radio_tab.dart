cf078245a43b5c2322d4ea0f5ec752ed93d903cc
Anas <anas.nader771@gmail.com>
2023-09-16 17:54:46 +0300

    finish all except localization

--- lib/home/radio/radio_tab.dart
+++ lib/home/radio/radio_tab.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../providers/app_config_providers.dart';
 
 class radioTab extends StatelessWidget {
 
 
   @override
   Widget build(BuildContext context) {
+    var provider =Provider.of<AppConfigProvider>(context);
+
     return Column(
       children: [
       Container(
@@ -16,9 +21,20 @@ class radioTab extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-            Icon(Icons.fast_rewind,size: 50,color: Theme.of(context).primaryColor),
-            Icon(Icons.play_arrow,size: 50,color: Theme.of(context).primaryColor),
-            Icon(Icons.fast_forward,size: 50,color: Theme.of(context).primaryColor),
+
+            Icon(Icons.fast_rewind,size: 50,color:
+           provider.isDarkMode()?
+               Theme.of(context).primaryColorDark
+               :
+            Theme.of(context).primaryColor),
+            Icon(Icons.play_arrow,size: 50,color: provider.isDarkMode()?
+            Theme.of(context).primaryColorDark
+                :
+            Theme.of(context).primaryColor),
+            Icon(Icons.fast_forward,size: 50,color:provider.isDarkMode()?
+            Theme.of(context).primaryColorDark
+                :
+            Theme.of(context).primaryColor),
           ],),
         )
 
